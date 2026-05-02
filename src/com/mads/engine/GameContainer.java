ab18e22af6346e92e7235ab750faad5d97586eb7
Matthew Greco <mgreco2112@tutanota.com>
2026-05-02 13:41:26 -0400

    Initial Commit

---
+++ src/com/mads/engine/GameContainer.java
@@ -0,0 +1,20 @@
+package com.mads.engine;
+
+public class GameContainer {
+
+    public GameContainer() {
+
+    }
+
+    public void start() {
+
+    }
+
+    public void stop() {
+
+    }
+
+    public void run() {
+
+    }
+}
