9c395d574a6c584ad8cfc4ca30e58f01c93eb860
Matthew Greco <mgreco2112@tutanota.com>
2026-05-03 17:42:56 -0400

    configured renderer to handle image tiles, demoed tile animation

--- src/com/mads/engine/Renderer.java
+++ src/com/mads/engine/Renderer.java
@@ -1,6 +1,7 @@
 package com.mads.engine;
 
 import com.mads.engine.gfx.Image;
+import com.mads.engine.gfx.ImageTile;
 
 import java.awt.image.DataBufferInt;
 
@@ -36,41 +37,34 @@ public class Renderer {
 
     public void drawImage(Image image, int offX, int offY) {
 
+        if (offX < -image.getW()) {
+            return;
+        }
+        if (offY < -image.getH()) {
+            return;
+        }
+        if (offX >= pW) {
+            return;
+        }
+        if (offY >= pH) {
+            return;
+        }
+
         int newX = 0;
         int newY = 0;
         int newWidth = image.getW();
         int newHeight = image.getH();
 
-        //no render code
-        if (offX < -newWidth) {
-            return;
-        }
-
-        if (offY < -newHeight) {
-            return;
-        }
-
-        if (offX >= pW) {
-            return;
-        }
-
-        if (offY >= pH) {
-            return;
-        }
-
         //clipping code
         if (offX < 0) {
             newX -= offX;
         }
-
         if (offY < 0) {
             newY -=offY;
         }
-
         if(newWidth + offX > pW) {
             newWidth -= newWidth + offX - pW;
         }
-
         if (newHeight + offY > pH) {
             newHeight -= newHeight + offY - pH;
         }
@@ -82,6 +76,56 @@ public class Renderer {
                         (x + offX),
                         (y + offY),
                         image.getP()[x + y * image.getW()]
+                );
+            }
+        }
+    }
+
+    public void drawImageTile(ImageTile image, int offX, int offY, int tileX, int tileY) {
+
+        if (offX < -image.getTileW()) {
+            return;
+        }
+        if (offY < -image.getTileH()) {
+            return;
+        }
+        if (offX >= pW) {
+            return;
+        }
+        if (offY >= pH) {
+            return;
+        }
+
+        int newX = 0;
+        int newY = 0;
+        int newWidth = image.getTileW();
+        int newHeight = image.getTileH();
+
+        //clipping code
+        if (offX < 0) {
+            newX -= offX;
+        }
+        if (offY < 0) {
+            newY -=offY;
+        }
+        if(newWidth + offX > pW) {
+            newWidth -= newWidth + offX - pW;
+        }
+        if (newHeight + offY > pH) {
+            newHeight -= newHeight + offY - pH;
+        }
+
+        //draw image
+        for (int y = newY; y < newHeight; y++) {
+            for (int x = newX; x < newWidth; x++) {
+                setPixel(
+                        (x + offX),
+                        (y + offY),
+                        image.getP()[
+                                  (x + tileX * image.getTileW())
+                                + (y + tileY * image.getTileH())
+                                * image.getW()
+                        ]
                 );
             }
         }
--- src/com/mads/game/GameManager.java
+++ src/com/mads/game/GameManager.java
@@ -4,15 +4,16 @@ import com.mads.engine.AbstractGame;
 import com.mads.engine.GameContainer;
 import com.mads.engine.Renderer;
 import com.mads.engine.gfx.Image;
+import com.mads.engine.gfx.ImageTile;
 
 import java.awt.event.KeyEvent;
 
 public class GameManager extends AbstractGame {
 
-    private Image image;
+    private ImageTile image;
 
     public GameManager() {
-        image = new Image("/test.png");
+        image = new ImageTile("/tile_test.png", 16, 16);
     }
 
     @Override
@@ -20,14 +21,26 @@ public class GameManager extends AbstractGame {
         if (gc.getInput().isKeyDown(KeyEvent.VK_A)) {
             System.out.println("A was pressed");
         }
+
+        temp += dt * 20;
+
+        if (temp > 3) {
+            temp = 0;
+        }
     }
+
+    float temp = 0;
 
     @Override
     public void render(GameContainer gc, Renderer r) {
-        r.drawImage(
+
+
+        r.drawImageTile(
                 image,
-                gc.getInput().getMouseX(),
-                gc.getInput().getMouseY()
+                gc.getInput().getMouseX() - 32,
+                gc.getInput().getMouseY() - 32,
+                (int) temp,
+                0
         );
     }
 
